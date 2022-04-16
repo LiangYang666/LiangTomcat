293f45ff7a2384aab2a5fce48befeb112f8d9ee0
LiangYang <mail.liangyang@gmail.com>
2022-04-16 22:54:05 +0800

    2.4.4 映射文件解析测试

---
+++ src/com/liang/XmlHandler.java
@@ -0,0 +1,62 @@
+package com.liang;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @Description: TODO
+ * @Author: LiangYang
+ * @Date: 2022/4/16 下午9:06
+ **/
+public class XmlHandler {
+    private final HashMap<String, HttpServlet> servletMapping = new HashMap<>();
+    private final HashMap<String, String> urlMapping = new HashMap<>();
+
+    public static void main(String[] args) {
+        String filePath ="web.xml";
+        XmlHandler xmlHandler = new XmlHandler(filePath);
+        System.out.println(xmlHandler.getServletMapping());
+        System.out.println(xmlHandler.getUrlMapping());
+    }
+
+    public XmlHandler(String filePath) {
+        try {
+            FileInputStream in = new FileInputStream(filePath);
+            SAXReader sr = new SAXReader();
+            Document doc = sr.read(in);
+            List<Element> elements = doc.getRootElement().elements();
+            for (Element element : elements) {
+                if (element.getName().equals("servlet-mapping")) {
+                    String servletName = element.elementTextTrim("servlet-name");
+                    String url = element.elementTextTrim("url-pattern");
+                    urlMapping.put(url, servletName);
+                } else if (element.getName().equals("servlet")) {
+                    String servletName = element.elementTextTrim("servlet-name");
+                    String className = element.elementTextTrim("servlet-class");
+                    HttpServlet HttpServlet = (HttpServlet) Class.forName(className).newInstance();
+                    servletMapping.put(servletName, HttpServlet);
+                }
+            }
+        }catch (FileNotFoundException | IllegalAccessException
+                | InstantiationException | ClassNotFoundException
+                | DocumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, HttpServlet> getServletMapping() {
+        return servletMapping;
+    }
+
+    public HashMap<String, String> getUrlMapping() {
+        return urlMapping;
+    }
+}
