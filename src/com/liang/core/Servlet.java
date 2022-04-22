0db3881b42347a4b4093a9a60be14dfa6cf174fd
LiangYang <mail.liangyang@gmail.com>
2022-04-22 19:04:59 +0800

    3.1.2 Servlet抽象类

--- src/com/liang/core/Servlet.java
+++ src/com/liang/core/Servlet.java
@@ -1,11 +1,12 @@
-package com.liang;
+package com.liang.core;
+
 
 /**
  * @Description: TODO
  * @Author: LiangYang
  * @Date: 2022/4/16 下午9:41
  **/
-public abstract class HttpServlet {
+public abstract class Servlet {
     public abstract void doGet(Request request, Response response);
     public abstract void doPost(Request request, Response response);
     public void service(Request request, Response response){
